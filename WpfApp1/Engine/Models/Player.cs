31491ab84413e1f8f4763a857c3c596ceec9dd72
Jakub A <lastferbbs@gmail.com>
2018-05-31 23:19:27 +0200

    Klasa Player

---
+++ WpfApp1/Engine/Models/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    class Player
+    {
+        string Imię { get; set; }
+        string KlasaPostaci { get; set: }
+        int PunktyZycia { get; set; }
+        int PunktyDoswiadczenia { get; set: }
+        int Poziom { get; set; }
+        int Złoto { get; set; }
+
+
+    }
+}
